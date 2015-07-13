2dbacba261d669d62ded551b5eeaaae7e31ebf65
sice81 <jaeikpark81@gmail.com>
2015-07-14 02:40:20 +0900

    /api/auth/signin 연동 확인

--- complete/src/main/java/com/genius/flashcard/Greeting.java
+++ complete/src/main/java/com/genius/flashcard/Greeting.java
@@ -1,4 +1,4 @@
-package hello;
+package com.genius.flashcard;
 
 public class Greeting {
 
--- complete/src/main/java/com/genius/flashcard/api/auth/controller/AuthController.java
+++ complete/src/main/java/com/genius/flashcard/api/auth/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.genius.flashcard.api.auth.controller;
 
 import java.util.logging.Logger;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,17 +10,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.genius.flashcard.api.auth.dto.User;
 
-@RestController()
-@RequestMapping("/api/auth")
+@RestController
+// @RequestMapping("/api/auth")
 public class AuthController {
 	Logger logger = Logger.getLogger(this.getClass().getName());
 
-	@RequestMapping(value="/signin", method=RequestMethod.POST)
-	public User signin(@RequestParam("userId") String userId, @RequestParam("password") String password) {
-		System.out.println("Hi2222!");
+	@RequestMapping(value = "/api/auth/signin", method = RequestMethod.POST)
+	// public User signin(@RequestParam("userId") String userId,
+	// @RequestParam("password") String password) {
+	public User signin(@RequestBody User user) {
+		System.out.println("/api/auth/signin");
+//		User user = new User();
+//		user.setUserId("u");
+//		user.setPassword("p");
+		return user;
+	}
+
+	@RequestMapping(value = "/test")
+	public User test() {
+		System.out.println("test");
 		User user = new User();
-		user.setUserId(userId);
-		user.setPassword(password);
-		return user; 
+		user.setUserId("userId");
+		return user;
 	}
 }
