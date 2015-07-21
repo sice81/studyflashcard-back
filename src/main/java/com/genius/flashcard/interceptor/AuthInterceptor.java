3c9aa0d221f17fa66cb1c363130d545b4a5a2ad1
sice81 <jaeikpark81@gmail.com>
2015-07-22 00:56:37 +0900

    auth

--- src/main/java/com/genius/flashcard/interceptor/AuthInterceptor.java
+++ src/main/java/com/genius/flashcard/interceptor/AuthInterceptor.java
@@ -1,5 +1,7 @@
 package com.genius.flashcard.interceptor;
 
+import java.util.logging.Logger;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -12,43 +14,42 @@ import org.springframework.web.servlet.ModelAndView;
 import com.genius.flashcard.auth.TokenService;
 
 public class AuthInterceptor implements HandlerInterceptor {
+	Logger logger = Logger.getLogger(this.getClass().getName());
+
 	@Autowired
 	TokenService tokenService;
-	
+
 	String[] allows = new String[]{"/api/auth/facebook"};
-	
+
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		System.out.println("preHandle()");
-		
-		
-		// TODO 뭔가 애매함... 정리가 필요...
 		String url = request.getRequestURI();
-		
+
 		// 인증 패스 목록 검사
 		for (String i : allows) {
 			if (i.equals(url)) {
 				return true;
 			}
 		}
-		
+
 		String accessToken = request.getParameter("accessToken");
 		if (accessToken == null) {
 			accessToken = request.getHeader("accessToken");
 		}
-		
+
 		if (tokenService.verify(accessToken) == false) {
 			response.setStatus(HttpStatus.UNAUTHORIZED.value());
+			return false;
 		}
-		
+
 		return true;
 	}
 
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
-		
+
 	}
 
 	@Override
