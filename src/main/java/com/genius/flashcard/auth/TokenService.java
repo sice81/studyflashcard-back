4f6b763258b4435ed0ea46154212bd113ed1f633
sice81 <jaeikpark81@gmail.com>
2015-07-23 23:23:56 +0900

    [auth] 토큰 만료시간 설정

--- src/main/java/com/genius/flashcard/auth/TokenService.java
+++ src/main/java/com/genius/flashcard/auth/TokenService.java
@@ -21,34 +21,34 @@ import net.sf.ehcache.config.CacheConfiguration;
 @Component
 public class TokenService {
 	Logger logger = Logger.getLogger(this.getClass().getName());
-	
+
 	KeyBasedPersistenceTokenService s = new KeyBasedPersistenceTokenService();
 	Ehcache cache;
-	
+
 	@Autowired
 	CacheManager cacheManager;
-	
+
 	public void setCacheManager(CacheManager cacheManager) {
 		this.cacheManager = cacheManager;
 	}
-	
+
 	@PostConstruct
 	public void init() {
 		logger.info("init()");
-		
+
 		CacheConfiguration cacheConfiguration = new CacheConfiguration("token", 10000);
 		cacheConfiguration.setOverflowToDisk(true);
 		cacheConfiguration.maxElementsOnDisk(10*10000);
 		cacheConfiguration.maxMemoryOffHeap("10M"); // 10MB
 		cacheConfiguration.setTimeToIdleSeconds(30*60); // 30min
-		cacheConfiguration.setTimeToLiveSeconds(30*60);
+//		cacheConfiguration.setTimeToLiveSeconds(10);
 		cache = new Cache(cacheConfiguration);
 		cacheManager.addCache(cache);
-		
-		
+
+
 		SecureRandomFactoryBean f = new SecureRandomFactoryBean();
 		f.setAlgorithm("SHA1PRNG");
-		
+
 		try {
 			s.setSecureRandom(f.createSecureRandom());
 		} catch (Exception e) {
@@ -57,67 +57,67 @@ public class TokenService {
 		s.setServerInteger(3238927);
 		s.setServerSecret("44gZAjJeTKHK9kZA4ij6CVAaEXds6j0T");
 	}
-	
+
 	public MyToken allocate(User user) {
 		MyToken token = new MyToken();
-		
+
 		Token t = s.allocateToken("");
 		String accessToken = t.getKey();
 
 		token.setToken(t);
 		token.setUser(user);
-		
+
 		Element e = new Element(accessToken, token);
 		cache.put(e);
-		
+
 		logger.info(String.format("allocated accessToken = %s", accessToken));
-		
+
 		return token;
 	}
-	
+
 	public boolean verify(String accessToken) {
 		logger.info(String.format("verify accessToken = %s", accessToken));
-		
+
 		if (accessToken == null) {
 			return false;
 		}
-		
+
 		Element e = cache.get(accessToken);
-		
+
 		if (e == null) {
 			return false;
 		}
-		
+
 		MyToken token = (MyToken) e.getObjectValue();
-		
+
 		if (token == null) {
 			return false;
 		}
-		
+
 		return true;
 	}
-	
+
 	public User getUser(String accessToken) {
 		logger.info(String.format("verify accessToken = %s", accessToken));
-		
+
 		User result = null;
-		
+
 		if (accessToken == null) {
 			return null;
 		}
-		
+
 		Element e = cache.get(accessToken);
-		
+
 		if (e == null) {
 			return null;
 		}
-		
+
 		MyToken token = (MyToken) e.getObjectValue();
-		
+
 		if (token != null) {
 			result = token.getUser();
 		}
-		
+
 		return result;
 	}
 }
