a83869c7e509ce4794708f579a13476c07798fdf
sice81 <jaeikpark81@gmail.com>
2015-08-07 22:43:56 +0900

    sh

--- startup.sh
+++ startup.sh
@@ -5,15 +5,14 @@ HOME=./
 PID_FILE=/tmp/genius-api-pid
 
 if [ -f ${PID_FILE} ]; then
-	echo "shutting down previous..."
-	PID=$(cat $PID_FILE)
-	kill $(PID)
+        echo "shutting down previous..."
+        PID=$(cat $PID_FILE);
+        kill $(PID);
+        rm $(PID_FILE)
 fi
 
 echo "Starting genius-api ..."
-if [ ! -f ${PID_FILE} ]; then
-	java -jar $(ls ./build/libs/*.jar) $(cat /home/ec2-user/argument.txt) &
-	echo $! > ${PID_FILE}
-	echo $!
-	echo "genius-api started ..."
-fi
+java -jar $(ls ./build/libs/*.jar) $(cat /home/ec2-user/argument.txt) &
+echo $! > ${PID_FILE}
+echo $!
+echo "genius-api started ..."
