1f686a22c0b667d247195128cc89c56eb79a3e8e
sice81 <jaeikpark81@gmail.com>
2015-08-07 23:38:45 +0900

    젠킨스 테스트

--- src/main/java/com/genius/flashcard/api/auth/controller/NopController.java
+++ src/main/java/com/genius/flashcard/api/auth/controller/NopController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class NopController {
 	@RequestMapping(value = "/nop", method = RequestMethod.GET)
 	public boolean nop() throws IOException {
-		return true;
+		return false;
 	}
 }
