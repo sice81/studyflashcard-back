328c42d03e9f0e01d2273102de9b8cca111ae240
sice81 <jaeikpark81@gmail.com>
2015-08-22 22:02:12 +0900

    [학습상태] 전부 맞음 처리한 경우 완료처리

--- src/main/java/com/genius/flashcard/api/v1/cardpacks/service/StudyStatusService.java
+++ src/main/java/com/genius/flashcard/api/v1/cardpacks/service/StudyStatusService.java
@@ -4,21 +4,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.genius.flashcard.api.auth.dto.User;
+import com.genius.flashcard.api.v1.cardpacks.dao.CardpackDao;
 import com.genius.flashcard.api.v1.cardpacks.dao.StudyStatusDao;
+import com.genius.flashcard.api.v1.cardpacks.dto.Cardpack;
 import com.genius.flashcard.api.v1.cardpacks.dto.StudyStatus;
 import com.genius.flashcard.api.v1.cardpacks.param.StudyStatusParam;
 import com.genius.flashcard.common.enums.StudyStatusCdEnum;
-
 
 @Service
 public class StudyStatusService {
 	@Autowired
 	StudyStatusDao studyStatusDao;
 
+	@Autowired
+	CardpackDao cardpackDao;
+
 	public void save(StudyStatusParam studyStatusParam, String cardpackId, String s3Key, String userId, User user) {
+		Cardpack cardpack = cardpackDao.get(cardpackId);
+
 		StudyStatus studyStatus = new StudyStatus();
 		studyStatus.setUserId(userId);
-		studyStatus.setStudyStatusCd(StudyStatusCdEnum.IN_STUDY);
+
+		// 학습완료 처리
+		if (studyStatusParam.getRights().length >= cardpack.getCardCnt()) {
+			studyStatus.setStudyStatusCd(StudyStatusCdEnum.COMPLETE);
+		} else {
+			studyStatus.setStudyStatusCd(StudyStatusCdEnum.IN_STUDY);
+		}
+
 		studyStatus.setWrongCnt(studyStatusParam.getWrongs().length);
 		studyStatus.setRightCnt(studyStatusParam.getRights().length);
 		studyStatus.setCardpackId(cardpackId);
